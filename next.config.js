17807cfa54c01d149b337392a8173f279bda31af
Nathan Davie <nathandavie@easymanifest.com>
2026-01-13 23:15:00 -0500

    PWA app fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const withSerwist = require("@serwist/next").default({
+    swSrc: "src/app/sw.ts",
+    swDest: "public/sw.js",
+    reloadOnOnline: true,
+    disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactCompiler: true,
 };
 
-module.exports = nextConfig;
+module.exports = withSerwist(nextConfig);
